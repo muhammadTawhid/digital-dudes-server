36fa4bfdef9a13af09762caf21241e89e020c16f
muhammadtawhid <muhammadtawhid.me@gmail.com>
2022-02-10 23:02:02 +0600

    database c r operation implemented

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 
 const express = require('express')
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express()
+app.use(cors());
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@digitaldudes.mq05e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const servicesCollection = client.db(process.env.DB_NAME).collection("services");
+
+  app.post("/addService", (req, res) => {
+    servicesCollection.insertOne(req.body)
+      .then(res => {
+        console.log(res);
+      })
+  })
+
+  app.get("/services", (req, res) => {
+    servicesCollection.find()
+      .toArray((err, doc) => {
+        res.send(doc)
+      })
+  })
+});
 
 app.get('/', (req, res) => {
   res.send('Hellooooooo World!')

a18d5f3dbb89fd17ac0dac692f00797fc1160768
muhammadtawhid <muhammadtawhid.me@gmail.com>
2022-02-27 05:18:10 +0600

    some update

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const { MongoClient, ObjectId } = require('mongodb');
 const stripe = require('stripe')("sk_test_51KUf9fENSw6iH46SRaIxsXtJCZlLDtvZGq43s2Wslv1Sb8FdJBXwd8YTWX8NKvtMXMjNsvWqmn6vL5IRro5DK5sE0087QIGvBd");
+const admin = require("firebase-admin");
+const { getAuth } = require('firebase-admin/auth');
+// import { getAuth, UserRecord } from 'firebase-admin/auth';
+var serviceAccount = require("./config/digital-dudes-agency-firebase-adminsdk-3vu5v-2c25ae7dab(1).json");
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
@@ -10,6 +15,10 @@ const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@digitaldudes.mq05e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -81,6 +90,15 @@ client.connect(err => {
       })
   })
 
+  app.get("/admins/:email", (req, res) => {
+    console.log(req.params.email);
+    adminsCollection.find({ adminEmail: req.params.email })
+      .toArray((err, doc) => {
+        res.send(doc)
+        console.log(doc);
+      })
+  })
+
   app.delete("/deleteAdmin/:id", (req, res) => {
     adminsCollection.findOneAndDelete({ _id: ObjectId(req.params.id) })
       .then((err, result) => {
@@ -88,104 +106,117 @@ client.connect(err => {
       })
   })
 
-  app.post("/addReview", (req, res)=>{
+  app.post("/addReview", (req, res) => {
     reviewsCollection.insertOne(req.body)
-    .then(result=> {
-      res.send(result)
-    })
+      .then(result => {
+        res.send(result)
+      })
   })
 
-  app.get("/reviews", (req, res) =>{
+  app.get("/reviews", (req, res) => {
     reviewsCollection.find()
-    .toArray((err, doc) =>{
-      res.send(doc)
-    })
+      .toArray((err, doc) => {
+        res.send(doc)
+      })
   })
 
-  app.post("/addPricing", (req, res) =>{
+  app.post("/addPricing", (req, res) => {
     pricingCollection.insertOne(req.body)
-    .then(result =>{
-      res.send(result)
-    })
+      .then(result => {
+        res.send(result)
+      })
   })
 
-  app.get("/pricing", (req, res) =>{
+  app.get("/pricing", (req, res) => {
     pricingCollection.find()
-    .toArray((err, doc) =>{
-      res.send(doc)
-    })
+      .toArray((err, doc) => {
+        res.send(doc)
+      })
   })
 
-  app.get("/pricing/:id", (req, res) =>{
-    pricingCollection.find({_id: ObjectId(req.params.id)})
-    .toArray((err, doc) =>{
-      res.send(doc[0])
-    })
+  app.get("/pricing/:id", (req, res) => {
+    pricingCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, doc) => {
+        res.send(doc[0])
+      })
   })
 
-  app.patch("/updatePricing/:id", (req, res) =>{
+  app.patch("/updatePricing/:id", (req, res) => {
     console.log(req.body);
-    pricingCollection.findOneAndUpdate({_id: ObjectId(req.params.id)}, {
+    pricingCollection.findOneAndUpdate({ _id: ObjectId(req.params.id) }, {
       $set: {
         pricingTitle: req.body.pricingTitle,
         pricingValue: req.body.pricingValue,
         services: req.body.services,
       }
     })
-    .then(result => {
-      res.send(result)
-    })
+      .then(result => {
+        res.send(result)
+      })
   })
 
-  app.post("/create-payment-intent", async(req, res) =>{
+  app.post("/create-payment-intent", async (req, res) => {
     const paymentInfo = req.body.pricingValue;
     const amount = paymentInfo;
     console.log(paymentInfo, "dsjkdkdjjfjfsssdddjj");
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
-      currency:"usd",
-      payment_method_types:["card"]
+      currency: "usd",
+      payment_method_types: ["card"]
     })
     console.log(paymentIntent.client_secret, "ddddddddadfsadfsdddddddddddd")
-    res.send({clientSecret: paymentIntent.client_secret})
+    res.send({ clientSecret: paymentIntent.client_secret })
   })
 
-  app.post("/addSubscriptedUser", (req, res) =>{
+  app.post("/addSubscriptedUser", (req, res) => {
     const paymentDetails = req.body;
     subscriptedUsersCollection.insertOne(paymentDetails)
-    .then(result =>{
-      res.send(result)
-    })
+      .then(result => {
+        res.send(result)
+      })
   })
 
   app.get("/subscriptedUser", (req, res) => {
     subscriptedUsersCollection.find()
-    .toArray((err, doc) =>{
-      res.send(doc)
-    })
+      .toArray((err, doc) => {
+        res.send(doc)
+      })
   })
 
   app.get("/subscriptedUser/:email", (req, res) => {
-    subscriptedUsersCollection.find({email: req.params.email})
-    .toArray((err, doc) =>{
-      res.send(doc[0])
-    })
+    const email = req.params.email;
+    const idToken = req.headers.authorization;
+    getAuth().verifyIdToken(idToken)
+      .then((decodedToken) => {
+        const deCodedEmail = decodedToken.email;
+        if (email === deCodedEmail) {
+          subscriptedUsersCollection.find({ email: email })
+            .toArray((err, doc) => {
+              res.send(doc[0])
+            })
+        }
+      })
+      .catch((error) => {
+        // Handle error
+      });
+
+
   })
 
-  app.put("/subscriptedUserNewPlan/:id", (req, res) =>{
+  app.put("/subscriptedUserNewPlan/:id", (req, res) => {
     const newSubscriptionDetail = req.body;
-    subscriptedUsersCollection.findOneAndReplace({_id: ObjectId(req.params.id)}, newSubscriptionDetail)
-    .then(result =>{
-      res.send(result)
-    })
+    subscriptedUsersCollection.findOneAndReplace({ _id: ObjectId(req.params.id) }, newSubscriptionDetail)
+      .then(result => {
+        res.send(result)
+      })
   })
 
   app.delete("/deleteSubscription/:id", (req, res) => {
-    subscriptedUsersCollection.findOneAndDelete({_id: ObjectId(req.params.id)})
-    .then(result => {
-      res.send(result)
-    })
+    subscriptedUsersCollection.findOneAndDelete({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result)
+      })
   })
 });
 
